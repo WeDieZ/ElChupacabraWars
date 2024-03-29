730d0dcfd578416e39f1739da5ff7fb59406be5e
WeDieZ <wediez09@gmail.com>
2024-03-29 21:55:46 +0700

    Beta_Build

--- Assets/Player/Scripts_All/PlayerController.cs
+++ Assets/Player/Scripts_All/PlayerController.cs
@@ -11,6 +11,7 @@ public class PlayerController : MonoBehaviour
     private float _fallVelocity = 0;
     private CharacterController _characterController;
     private Vector3 _VectorMove;
+    public Animator animator;
     void Start()
     {
         _characterController = GetComponent<CharacterController>();
@@ -31,38 +32,55 @@ public class PlayerController : MonoBehaviour
     }
 
     void Update()
-    { 
-        if (_characterController.isGrounded)
-        {
-            can_jump = 2;
-        }
+    {
+        Jump();
 
-        if (Input.GetKeyDown(KeyCode.Space) && (can_jump >=1))
-        {
-            _fallVelocity = -jumpforce;
-            can_jump -= 1;
-        }
+        Walk_Move();
+    }
 
+    private void Walk_Move()
+    {
         _VectorMove = Vector3.zero;
+        var runDirection = 0;
 
         if (Input.GetKey(KeyCode.W))
         {
             _VectorMove += transform.forward;
+            runDirection = 1;
         }
 
         if (Input.GetKey(KeyCode.A))
         {
             _VectorMove -= transform.right;
+            runDirection = 3;
         }
 
         if (Input.GetKey(KeyCode.D))
         {
             _VectorMove += transform.right;
+            runDirection = 4;
         }
 
         if (Input.GetKey(KeyCode.S))
         {
             _VectorMove -= transform.forward;
+            runDirection = 2;
+        }
+
+        animator.SetInteger("RunDirection", runDirection);
+    }
+
+    private void Jump()
+    {
+        if (_characterController.isGrounded)
+        {
+            can_jump = 2;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space) && (can_jump >= 1))
+        {
+            _fallVelocity = -jumpforce;
+            can_jump -= 1;
         }
     }
 }
--- Assets/Scenes/Scripts/Ingridients/Cheese.cs
+++ Assets/Scenes/Scripts/Ingridients/Cheese.cs
@@ -5,12 +5,14 @@ using UnityEngine;
 public class Cheese : MonoBehaviour
 {
     public GameObject Boss;
+    public GameObject Barrier;
     public void OnTriggerEnter(Collider other)
     {
         if (other.tag == "Player")
         {
             Destroy(gameObject);
             Boss.SetActive(true);
+            Barrier.SetActive(false);
         }
     }
 }
